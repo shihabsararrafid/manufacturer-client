4b49560822ff9da691cc7924a40fd8a4ab7ba393
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-05-29 00:23:25 +0600

    new portion slat added

---
+++ src/Components/Pages/Home/Banner.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Slat from "./Slat";
+
+const Banner = () => {
+  return (
+    <div
+      class="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://t3.ftcdn.net/jpg/00/89/10/68/360_F_89106812_D8J6Y7KzgLr1FKTgOYZKYSM458BXTCaX.jpg)",
+      }}
+    >
+      <div class="hero-overlay bg-opacity-60"></div>
+      <div class="hero-content  text-neutral-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+          <p class="mb-5">
+            <span className="text-2xl uppercase text-secondary">Toolex</span> is
+            a reputed manufacturer company in{" "}
+            <span className="uppercase"> bangladesh </span> specially for{" "}
+            <span className="uppercase text-secondary">Carpentry tools</span>.
+          </p>
+
+          <button class="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Banner from "./Banner";
 import Navbar from "./Navbar";
+import Slat from "./Slat";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Banner></Banner>
+      <Slat></Slat>
+    </div>
+  );
 };
 
 export default Home;
