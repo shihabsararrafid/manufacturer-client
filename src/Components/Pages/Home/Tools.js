da730b1fa34564f3cd5f8d685967f63078fa8014
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-05-29 07:25:17 +0600

    loaded from heoku

--- src/Components/Pages/Home/Tools.js
+++ src/Components/Pages/Home/Tools.js
@@ -4,18 +4,18 @@ import Tool from "./Tool";
 const Tools = () => {
   const [tools, setTools] = useState([]);
   useEffect(() => {
-    fetch("products.json")
+    fetch("https://toolex-factory.herokuapp.com/tools")
       .then((res) => res.json())
       .then((data) => setTools(data));
   }, []);
   return (
-    <div>
+    <div className="my-10">
       <h1 className="text-6xl mx-7 my-9 uppercase font-bold text-secondary lg:text-left text-center">
         Tools
       </h1>
       <div className="grid mx-7 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {tools.map((tool) => (
-          <Tool tool={tool} len="5"></Tool>
+        {tools.slice(0, 6).map((tool) => (
+          <Tool key={tool._id} tool={tool} len="5"></Tool>
         ))}
       </div>
     </div>
