51fb43a8f78d52b1ca9e1ddd3d06d110b415bef5
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-05-30 10:44:54 +0600

    my order added

--- src/Components/Pages/Dashboard/MyOrders.js
+++ src/Components/Pages/Dashboard/MyOrders.js
@@ -1,11 +1,105 @@
-import React from "react";
+import { data } from "autoprefixer";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const MyOrders = () => {
+  const [user, loading, error] = useAuthState(auth);
+
+  const [orders, setOrders] = useState([]);
+  const [products, setProducts] = useState([]);
+  // const [orderId, setOrderId] = useState([]);
+  let orderId = [];
+  let neworders = [];
+  const [ordersnew, setOrdersnew] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/myorders?email=${user?.email}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setOrders(data);
+        //     for (const id of data) {
+        //       if (orderId.indexOf(id._id) === -1) {
+        //         orderId.push(id._id);
+        //       }
+
+        //       const newUrl = `http://localhost:5000/purchase/${id._id}`;
+        //       fetch(newUrl)
+        //         .then((res) => res.json())
+        //         .then((data) => {
+        //           if (neworders.indexOf(data) === -1) {
+        //             neworders.push(data);
+        //             //console.log(...neworders);
+        //             setOrdersnew(neworders);
+        //           }
+
+        //           //   ordersnew.push(data);
+        //           //console.log(ordersnew);
+        //           //neworders = [...ordersnew, data];
+        //           //console.log(neworders);
+        //           //setOrdersnew(neworders);
+        //         });
+
+        //       //fetch();
+        //     }
+        //     // console.log(orderId);
+        //     // console.log(neworders);
+        //   });
+        //setOrdersnew(neworders);
+      });
+  }, [user?.email]);
+  useEffect(() => {
+    fetch("https://toolex-factory.herokuapp.com/tools")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  //console.log(orderId);
+  console.log(orders);
+
+  for (let i = 0; i < orders?.length; i++) {
+    const newer = products.find((p) => p._id === orders[i]?._id);
+    ordersnew.push(newer);
+    //console.log(order);
+  }
+
   return (
     <div>
-      <h1>My Orders</h1>
+      <h1>Hello{orders.length}</h1>
+      <div className="grid gap-10 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+        {orders.map((o) => (
+          <div>
+            <div class="card  card-side bg-base-100 shadow-xl ">
+              <figure>
+                <img
+                  className="w-[150px] mask mask-circle text-center"
+                  src={o.img}
+                  alt="Movie"
+                ></img>
+              </figure>
+              <div class="card-body font-mono">
+                <h2 class="card-title text-left  uppercase">{o.name}</h2>
+                <div className="font-semibold">
+                  <p>Ordered Quantity:{o.quantity}</p>
+                  <p>Price:{o.price}</p>
+                  <p>
+                    Amount To be Paid:{" "}
+                    <span className="text-secondary font-bold">
+                      {parseInt(o.price) * parseInt(o.quantity)}
+                    </span>
+                  </p>
+                </div>
+                <div class="card-actions justify-center">
+                  <button class="btn btn-secondary">Pay Now</button>
+                  <button className="bg-red-600 text-white btn">
+                    Delete Item
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
 export default MyOrders;
--- src/Components/Pages/Purchase/SingTool.js
+++ src/Components/Pages/Purchase/SingTool.js
@@ -29,7 +29,7 @@ const SingTool = () => {
     if (quantity < minQuantity) {
       alert("Quantity cannot be less than min quantity");
     } else {
-      const url = `http://localhost:5000/purchase/${_id.id}?email=${user?.email}&quantity=${quantity}`;
+      const url = `http://localhost:5000/purchase/${_id.id}?email=${user?.email}&quantity=${quantity}&price=${tool.price}&img=${tool.img}&name=${tool.name}`;
       console.log(url);
       fetch(url, {
         method: "PUT",
